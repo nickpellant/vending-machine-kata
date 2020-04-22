0870b33d35e2c5fc4f892bfb8b4c813c49a5b1cf
Nick Pellant <nick@nickpellant.com>
2020-04-22 22:40:08 +0100

    Implement CallableLoader
    We want our auto injection of dependency to recognise callable services.

--- system/container.rb
+++ system/container.rb
@@ -2,10 +2,18 @@
 
 require 'dry/system/container'
 
+# Recognise callable objects with dependency injection
+class CallableLoader < Dry::System::Loader
+  def call(*args)
+    constant.respond_to?(:call) ? constant : constant.new(*args)
+  end
+end
+
 # Application dependency container
 class App < Dry::System::Container
   configure do |config|
     config.auto_register = 'lib'
+    config.loader = CallableLoader
   end
 
   load_paths!('lib')
