688a30fe8cb912d5c914f051e5b54b7a9da2086a
Nick Pellant <nick@nickpellant.com>
2020-04-22 18:31:45 +0100

    Auto register ROM persistence namespace

--- system/boot/database.rb
+++ system/boot/database.rb
@@ -9,6 +9,11 @@ App.boot(:database) do
 
   start do
     rom_configuration = ROM::Configuration.new(:sql, ENV.fetch('DATABASE_URL'))
+
+    rom_configuration.auto_registration(
+      File.join(Dir.pwd, '/lib/persistence'), namespace: 'Persistence'
+    )
+
     register(:database, ROM.container(rom_configuration))
   end
 end
