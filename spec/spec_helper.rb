42cfafd7a8f5a8a09434c72234fc1ccf8e0365e3
Nick Pellant <nick@nickpellant.com>
2020-04-22 18:40:22 +0100

    Configure factory support for RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ ENV['APP_ENV'] ||= 'test'
 
 require_relative '../run'
 
+require_relative 'support/factory'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
