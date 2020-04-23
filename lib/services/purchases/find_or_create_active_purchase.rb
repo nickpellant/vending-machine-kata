31177466caca3ed9e3f1a6e012ad7e40a69fdd36
Nick Pellant <nick@nickpellant.com>
2020-04-23 14:58:40 +0100

    Add FindOrCreateActivePurchase service

---
+++ lib/services/purchases/find_or_create_active_purchase.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'services/application_service'
+
+module Services
+  module Purchases
+    # Find or create active purchase in database
+    class FindOrCreateActivePurchase < ApplicationService
+      include Import['repos.purchase_repo']
+
+      def call
+        purchase = purchase_repo.active
+        return purchase if purchase
+
+        purchase_repo.create(state: :active)
+      end
+    end
+  end
+end
