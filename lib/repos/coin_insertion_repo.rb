26f98cc6e252c235d1769e21a460e0e5821d88b9
Nick Pellant <nick@nickpellant.com>
2020-04-23 13:39:58 +0100

    Add CoinInsertionRepo#count_by_coin
    Count how many coins of a certain type have been inserted.

--- lib/repos/coin_insertion_repo.rb
+++ lib/repos/coin_insertion_repo.rb
@@ -6,5 +6,9 @@ module Repos
   # Convenience interface for coin insertion relations
   class CoinInsertionRepo < ApplicationRepo[:coin_insertions]
     commands(:create)
+
+    def count_by_coin(coin)
+      coin_insertions.where(coin_id: coin.id).count
+    end
   end
 end
