c42a926f387c11e596b877c94afe6de7de3d1561
Nick Pellant <nick@nickpellant.com>
2020-04-23 13:29:23 +0100

    Add CoinInsertions relation

--- lib/persistence/relations/coins.rb
+++ lib/persistence/relations/coins.rb
@@ -4,7 +4,11 @@ module Persistence
   module Relations
     # Coins database interface
     class Coins < ROM::Relation[:sql]
-      schema(:coins, infer: true)
+      schema(:coins, infer: true) do
+        associations do
+          has_many :coin_insertions
+        end
+      end
 
       auto_struct(true)
     end
